7aa811dda01497d3bae4bd08bf9e1dce96246cd0
beam_former <beam_former@noreply.invalid>
2024-01-29 16:21:27 -0700

    Update spliffy to 1.2.0 switch spliffy to use postgres instead of pg

--- frameworks/JavaScript/spliffy/db/postgres.js
+++ frameworks/JavaScript/spliffy/db/postgres.js
@@ -1,44 +1,29 @@
-const { Client, Pool } = require( 'pg' ).native;
-const cpus = require( 'os' ).cpus().length
+const postgres = require( 'postgres' );
 
-let clientOpts = {
+const sql =postgres({
     host: process.env.DB_HOST || 'localhost',
     user: 'benchmarkdbuser',
     password: 'benchmarkdbpass',
-    database: 'hello_world'
-};
-
-let pool
-
-const query = async ( text, values ) => ( await pool.query( text, values || undefined ) ).rows;
+    database: 'hello_world',
+    max: 1
+});
 
 module.exports = {
-    async init() {
-        const client = new Client( clientOpts )
-        await client.connect()
-        const res = await client.query( 'SHOW max_connections' )
-        let maxConnections = Math.floor( res.rows[0].max_connections * 0.9 / cpus )
-        //1 worker per cpu, each worker pool gets a fraction of the max connections
-        //only use 90% to avoid too many clients errors
-        pool = new Pool( Object.assign( { ...clientOpts }, { max: maxConnections } ) )
-        pool.on( 'error', ( err ) => {
-            console.error( 'Unexpected client error', err )
-        } )
-        await client.end()
-    },
+    async init() {},
     allFortunes: async () =>
-        query( 'SELECT * FROM fortune' ),
+        await sql`SELECT id, message FROM fortune`,
 
     worldById: async ( id ) =>
-        query( 'SELECT * FROM world WHERE id = $1', [id] )
-            .then( arr => arr[0] ),
+        await sql`SELECT id, randomNumber FROM world WHERE id = ${id}`.then( arr => arr[0] ),
 
     allWorlds: async () =>
-        query( 'SELECT * FROM world' ),
+        sql`SELECT * FROM world`,
 
-    bulkUpdateWorld: async worlds => Promise.all(
-        worlds.map( world =>
-            query( 'UPDATE world SET randomnumber = $1 WHERE id = $2',
-                [world.randomnumber, world.id] ) )
-    ).then( () => worlds )
+    bulkUpdateWorld: async worlds => await  sql `UPDATE world SET randomnumber = (update_data.randomNumber)::int FROM (
+                    VALUES ${sql( 
+                        worlds.map(world => [world.id, world.randomNumber])
+                                .sort((a, b) => (a[0] < b[0]) ? -1 : 1)
+                    )}) AS update_data (id,randomNumber)
+                    WHERE world.id = (update_data.id)::int
+                `
 }
